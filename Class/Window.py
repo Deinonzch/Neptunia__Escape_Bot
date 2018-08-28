40eca0ea2d271bc563647d22139d044932f5fe81
Deinonzch <Deinonzch@users.noreply.github.com>
2018-08-28 18:25:44 +0200

    Add class window

---
+++ Class/Window.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+
+
+def events(bot):
+    for event in pygame.event.get():
+        keyboard_events(event.type, bot)
+        clicked_exit_button_of_window(event.type, bot)
+        print('I work')
+
+
+def keyboard_events(event_type, bot):
+    if event_type == pygame.KEYDOWN:
+        exit_if_q(bot)
+
+
+def exit_if_q(bot):
+    if pygame.key.get_pressed()[pygame.K_q]:
+        pygame.quit()
+        bot.finish()
+        sys.exit(0)
+
+
+def clicked_exit_button_of_window(event_type, bot):
+    if event_type == pygame.QUIT:
+        pygame.quit()
+        bot.finish()
+        sys.exit(0)
+
+
+def events_control():
+    while True:
+        events()
