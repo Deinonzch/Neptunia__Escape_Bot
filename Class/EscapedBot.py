a38a305dfbd0bf2372541722408e130f8e0e11fe
Deinonzch <Deinonzch@users.noreply.github.com>
2018-08-28 17:49:42 +0200

    Improve exit from a program

---
+++ Class/EscapedBot.py
@@ -0,0 +1,22 @@
+from Class.KeyboardSimulation import *
+import time
+
+
+class EscapedBot:
+    def __init__(self):
+        self.undone = True
+
+    def finish(self):
+        self.undone = False
+
+    def escape(self):
+        while self.undone:
+            time.sleep(8.5)
+            PressKey(R)
+            time.sleep(.5)
+            ReleaseKey(R)
+            time.sleep(.5)
+            PressKey(L)
+            time.sleep(.5)
+            ReleaseKey(L)
+            print(self.undone)
--- Main.py
+++ Main.py
@@ -1,26 +1,20 @@
-import time
 from Class.Window import *
-from Class.KeyboardSimulation import *
+import threading
+from Class.EscapedBot import *
 
 pygame.init()
 
 size = [500, 700]
+
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("My Game")
 
-pygame.init()
+bot = EscapedBot()
+
+t = threading.Thread(target=bot.escape, name='thread1')
+
+t.start()
 
 while True:
-    time.sleep(8.5)
-    PressKey(R)
-    events()
-    time.sleep(.5)
-    ReleaseKey(R)
-    events()
-    time.sleep(.5)
-    PressKey(L)
-    events()
-    time.sleep(.5)
-    ReleaseKey(L)
-    events()
+    events(bot)
