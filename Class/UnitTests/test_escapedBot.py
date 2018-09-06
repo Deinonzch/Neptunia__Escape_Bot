03fa6b2dfe23dc6e0bae9fc4b7181d8f21dbdbe2
Deinonzch <Deinonzch@users.noreply.github.com>
2018-09-06 15:22:05 +0200

    Refactor test_init and test_finish Add test_stoped and test_restarted

--- Class/UnitTests/test_escapedBot.py
+++ Class/UnitTests/test_escapedBot.py
@@ -1,13 +1,28 @@
 from unittest import TestCase
 from Class import EscapedBot
+import keyboard
+import threading
 
 
 class TestEscapedBot(TestCase):
     def test_init(self):
         bot = EscapedBot.EscapedBot()
-        self.assertEqual(bot.undone, True)
+        self.assertEqual(bot.undone, True, 'undone is not True')
+        self.assertEqual(bot.stop, False, 'stop is not False')
 
-    def test_finish(self):
+    def test_finished(self):
         bot = EscapedBot.EscapedBot()
-        bot.finish()
-        self.assertEqual(bot.undone, False)
+        bot.finished()
+        self.assertEqual(bot.undone, False, 'undone is not false')
+
+    def test_stoped(self):
+        bot = EscapedBot.EscapedBot()
+        bot.stoped()
+        self.assertEqual(bot.stop, True, 'stop is not True')
+
+    def test_restarted(self):
+        bot = EscapedBot.EscapedBot()
+        bot.stoped()
+        self.assertEqual(bot.stop, True, 'stop is not True')
+        bot.restarted()
+        self.assertEqual(bot.stop, False, 'stop is not False')
