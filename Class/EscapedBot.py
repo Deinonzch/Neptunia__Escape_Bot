5868841ec97e7ace773a33ec25d1e5ab76b559bd
Deinonzch <Deinonzch@users.noreply.github.com>
2018-09-13 13:10:15 +0200

    Refactor

--- Class/EscapedBot.py
+++ Class/EscapedBot.py
@@ -1,5 +1,5 @@
 import keyboard
-from Class.ImageRecognition import get_menu_attack_text, is_first_menu_attack, is_defend, is_escape
+from Class.ImageRecognition import get_menu_attack_text, is_first_menu_attack, is_escape
 
 
 class EscapedBot:
@@ -7,32 +7,39 @@ class EscapedBot:
         self.undone = True
         self.stop = False
 
+    def listener(self):
+        while self.undone:
+            self.if_end_key_is_pressed_work_is_done()
+            self.if_stop_key_is_pressed_do_break()
+            self.if_restart_key_is_pressed_back_to_work()
+
+    def if_end_key_is_pressed_work_is_done(self):
+        if keyboard.is_pressed('q'):
+            self.finished()
+
     def finished(self):
         self.undone = False
+
+    def if_stop_key_is_pressed_do_break(self):
+        if keyboard.is_pressed('alt+x'):
+            self.stoped()
 
     def stoped(self):
         self.stop = True
 
+    def if_restart_key_is_pressed_back_to_work(self):
+        if keyboard.is_pressed('alt+z'):
+            self.restarted()
+
     def restarted(self):
         self.stop = False
-
-    def listener(self):
-        while self.undone:
-            self.press_end_key()
-            self.press_stop_key()
-            self.press_restart_key()
 
     def escape(self):
         while self.undone:
             texts = get_menu_attack_text()
             self.change_attack_menu_if_is_first(texts)
-            self.is_escape_button(texts[3])
+            self.if_attack_menu_have_escape_button_excape(texts[3])
             self.have_break()
-
-    @staticmethod
-    def defend_move(text):
-        if is_defend(text):
-            keyboard.press_and_release('r')
 
     @staticmethod
     def change_attack_menu_if_is_first(texts):
@@ -40,22 +47,14 @@ class EscapedBot:
             keyboard.press_and_release('r')
 
     @staticmethod
-    def is_escape_button(text):
+    def if_attack_menu_have_escape_button_excape(text):
         if is_escape(text):
-            keyboard.press_and_release('l')
-
-    def press_end_key(self):
-        if keyboard.is_pressed('q'):
-            self.finished()
-
-    def press_stop_key(self):
-        if keyboard.is_pressed('alt+x'):
-            self.stoped()
-
-    def press_restart_key(self):
-        if keyboard.is_pressed('alt+z'):
-            self.restarted()
+            escape()
 
     def have_break(self):
         while self.stop and self.undone:
             print('I have a break')
+
+
+def escape():
+    keyboard.press_and_release('l')
