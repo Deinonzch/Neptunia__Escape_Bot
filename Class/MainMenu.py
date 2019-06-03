677289a5db54a8d3a73c470bffd2d20a1cf731c0
Deinonzch <Deinonzch@users.noreply.github.com>
2019-06-03 21:35:06 +0200

    Refactor MainMenu

--- Class/MainMenu.py
+++ Class/MainMenu.py
@@ -1,4 +1,4 @@
-from Class.EscapeBotMenu import *
+from Class.ListOfBotsMenu import *
 from Class.Menu import *
 import time
 
@@ -8,21 +8,22 @@ class MainMenu(Menu):
         super().__init__(surface)
         self.__start_button = Button(surface, [size[0] / 2, (size[1] - 100) / 2], "Run")
         self.__exit_button = Button(surface, [size[0] / 2, (size[1]) / 2], "Exit")
-        self.__escape_bot_menu = EscapeBotMenu(surface, size)
+        self.__list_of_bots_menu = ListOfBotsMenu(surface, size)
 
     def events(self):
         for event in pygame.event.get():
-            self._clicked_run_button(event.type)
             self._clicked_exit_button(event.type)
             self.clicked_exit_button_of_window(event.type)
+            self._clicked_run_button(event.type)
 
     def _clicked_run_button(self, event_type):
         if event_type == pygame.MOUSEBUTTONDOWN:
             self.__start_button.clicked()
             self.draw()
             time.sleep(0.1)
-            self._go_to_escape_bot_menu()
+            self._go_to_list_of_bot_menu()
             self.__start_button.unclicked()
+            self.draw()
 
     def _clicked_exit_button(self, event_type):
         if event_type == pygame.MOUSEBUTTONDOWN:
@@ -37,12 +38,12 @@ class MainMenu(Menu):
         self.__exit_button.draw()
         pygame.display.flip()
 
-    def _go_to_escape_bot_menu(self):
+    def _go_to_list_of_bot_menu(self):
         if self.__start_button.is_clicked():
-            self.__escape_bot_menu.run()
-        self.__escape_bot_menu.draw()
-        while self.__escape_bot_menu.is_done():
-            self.__escape_bot_menu.events()
+            self.__list_of_bots_menu.run()
+        self.__list_of_bots_menu.draw()
+        while self.__list_of_bots_menu.is_done():
+            self.__list_of_bots_menu.events()
 
     def _exit(self):
         if self.__exit_button.is_clicked():
